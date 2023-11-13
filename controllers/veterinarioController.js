c6079c68a4f08d98eaee51380cb20cdb2310e29e
IsGhost <jamaica.1999@hotmail.com>
2023-11-13 13:44:03 -0400

    bug fixed emailRegistro

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -1,7 +1,7 @@
 import generarJWT from "../helpers/generarJWT.js";
 import Veterinario from "../models/Veterinario.js";
 import generarId from "../helpers/generarId.js";
-import emailRegistro from "../helpers/EmailRegistro.js";
+import emailRegistro from "../helpers/emailRegistro.js";
 import emailOlvidePassword from "../helpers/emailOlvidePassword.js";
 
 
